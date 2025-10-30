0816cd4dc5ad7f8d22e7c2124618b66521499fab
rohan-intellimedia <rohanm@intellimedianetworks.net>
2025-10-30 19:16:44 +0530

    Used middlewares

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,24 @@ import cors from 'cors';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import authRoutes from './routes/auth.route';
+import petRoutes from './routes/pet.route';
+import applicationRoutes from './routes/application.route';
+import { errorHandler } from './middlewares/error.middleware';
 
 const app = express();
 
 app.use(cors());
 app.use(json());
 
-//routes
+// routes
 app.use('/api/auth', authRoutes);
+app.use('/api/pets', petRoutes);
+app.use('/api/applications', applicationRoutes);
 
 // health
 app.get('/health', (req, res) => res.json({ ok: true }));
 
+// global error handler
+app.use(errorHandler);
 
 export default app;
